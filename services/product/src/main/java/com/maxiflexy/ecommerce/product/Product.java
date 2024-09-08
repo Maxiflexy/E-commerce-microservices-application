326ff9915b5a0e30e896ab195a34c07f871be338
Maxiflexy <onyemax247@gmail.com>
2024-09-08 09:53:05 +0100

    updated product service

---
+++ services/product/src/main/java/com/maxiflexy/ecommerce/product/Product.java
@@ -0,0 +1,28 @@
+package com.maxiflexy.ecommerce.product;
+
+import com.maxiflexy.ecommerce.category.Category;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.math.BigDecimal;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Getter
+@Setter
+@Entity
+public class Product {
+
+    @Id
+    @GeneratedValue
+    private Integer id;
+    private String name;
+    private String description;
+    private double availableQuantity;
+    private BigDecimal price;
+
+    @ManyToOne
+    @JoinColumn(name = "category_id")
+    private Category category;
+}
