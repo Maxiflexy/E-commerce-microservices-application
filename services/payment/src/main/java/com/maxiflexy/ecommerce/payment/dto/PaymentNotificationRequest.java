e53c7b0463676365c22d1a12272f4bdc811ae01f
Maxiflexy <onyemax247@gmail.com>
2024-09-21 22:37:46 +0100

    created notification

---
+++ services/payment/src/main/java/com/maxiflexy/ecommerce/payment/dto/PaymentNotificationRequest.java
@@ -0,0 +1,15 @@
+package com.maxiflexy.ecommerce.payment.dto;
+
+import com.maxiflexy.ecommerce.payment.enums.PaymentMethod;
+
+import java.math.BigDecimal;
+
+public record PaymentNotificationRequest(
+        String orderReference,
+        BigDecimal amount,
+        PaymentMethod paymentMethod,
+        String customerFirstname,
+        String customerLastname,
+        String customerEmail
+) {
+}
--- services/payment/src/main/java/com/maxiflexy/ecommerce/payment/service/PaymentService.java
+++ services/payment/src/main/java/com/maxiflexy/ecommerce/payment/service/PaymentService.java
@@ -3,5 +3,6 @@ package com.maxiflexy.ecommerce.payment.service;
 import com.maxiflexy.ecommerce.payment.dto.PaymentRequest;
 
 public interface PaymentService {
+
     Integer createPayment(PaymentRequest paymentRequest);
 }
